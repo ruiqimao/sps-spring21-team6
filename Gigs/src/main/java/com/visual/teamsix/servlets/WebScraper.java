ce84b96a9d620cdbdb52e9b05c5703705b1077bc
Joshua Kiprono <joshuakiprono8@gmail.com>
2021-04-01 01:38:58 +0000

    new changes

--- Gigs/src/main/java/com/visual/teamsix/servlets/WebScraper.java
+++ Gigs/src/main/java/com/visual/teamsix/servlets/WebScraper.java
@@ -1,12 +1,10 @@
 package com.visual.teamsix.servlets;
 
 import java.io.IOException;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import resources.GetWebData;
 
 /** Servlet that responds with data retrieved from webscrapper. */
@@ -14,11 +12,6 @@ import resources.GetWebData;
 public class WebScraper extends HttpServlet {
     private static final long serialVersionUID = 1L;
     GetWebData scrap = new GetWebData();
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json;");
-        
-    }
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json;");
