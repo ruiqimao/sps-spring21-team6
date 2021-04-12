c5f51bb72eb7f630f46e2c4380914709d42b4e91
Gginne <23042760+Gginne@users.noreply.github.com>
2021-04-12 18:05:43 -0400

    Client server (#8)
    * Created skeleton react app
    
    * Created baseline components for main page
    
    * Created List item for sideview list
    
    * Created static scrollbar for Sideview
    
    * Added to base
    
    * Made code style changes
    
    * Adapted web scraper to obtain gigs based on ZIP location and distance
    
    * Added salary field to scraped gig data
    
    * Remade Data class into Gig model with constructor
    
    * Adapted scraper to new Gig model
    
    * Created helper to get data of current address
    
    * Fetched servlet data with react
    
    * Updated ListItem component to display servlet data
    
    * Created servlet request with dynamic location
    
    * Enforced paginaion limit depending on current job count
    
    * Fixed scraped gig links
    
    * Fixed code and made retouches
    
    * Removed java compilation files
    
    * testing deployment
    
    * Resolved server loading issues
    
    Co-authored-by: Gginne <23042760+Gginne@users.nonreply.github.com>

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SideView from "../components/SideView";
 import MapView from "../components/MapView";
+import {usePosition, getAddressData} from "../map/"
 
 function Main() {
+
+    const {coords, setCoords, err} = usePosition()
+    const [radius, setRadius] = useState(8050)
+    const [gigs, setGigs] = useState([])
+
+    useEffect(() => { 
+        async function fetchGigs(){
+            try{
+                const address = await getAddressData(coords.lat, coords.lng);
+                const servletRes = await fetch(`http://localhost:8080/get?zip=${address.zip.long_name}&rad=5`);
+                const gigs = await servletRes.json();
+                setGigs(gigs.data.subdata);
+            } catch(err){
+                console.error(err);
+            }
+            
+        }
+        
+        if(gigs.length > 0) return;
+        fetchGigs();
+
+    }, [coords, gigs])
+
+    
     return (
         
         <div className="row">
             
-            <SideView />
+            <SideView gigs={gigs}/>
         
-            <MapView />
+            <MapView coords={coords} radius={radius}/>
             
         </div>
     
